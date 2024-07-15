51520c8d75c11faa13abc50c07f6190523f1c1b0
Etienne Païta <etienne@studio-equinoxe.com>
2024-07-15 13:55:30 +0200

    Add top bar on match tab

--- lib/presentation/screens/coach/match/match_screen.dart
+++ lib/presentation/screens/coach/match/match_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_football/config/app_colors.dart';
 import 'package:flutter_football/domain/models/match_details.dart';
 import 'package:flutter_football/presentation/blocs/match/match_bloc.dart';
 import 'package:flutter_football/presentation/blocs/match/match_event.dart';
@@ -37,72 +38,85 @@ class _MatchScreenState extends State<MatchScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Builder(builder: (context) {
-      return BlocListener<MatchBloc, MatchState>(
-        listener: (context, state) {
-          switch (state.status) {
-            case MatchStatus.loading:
-              LoadingDialog.show(context);
-              break;
-            case MatchStatus.error:
-              LoadingDialog.hide(context);
-              ErrorDialog.show(context, state.error?.message ?? "Une erreur est survenue...");
-              break;
-            case MatchStatus.success:
-              LoadingDialog.hide(context);
-              break;
-            case MatchStatus.initial:
-              LoadingDialog.hide(context);
-              break;
-            case MatchStatus.refresh:
-              BlocProvider.of<MatchBloc>(context).add(GetMatches());
-              break;
-            case MatchStatus.redirect:
-              final redirection = state.redirection;
-              //LoadingDialog.show(context);
-              Future.delayed(Duration(seconds: 2), () {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.white,
+        ),
+        title: Text(
+          "Match",
+          style: TextStyle(color: AppColors.white),
+        ),
+        backgroundColor: currentAppColors.secondaryColor,
+        centerTitle: true,
+      ),
+      body: Builder(builder: (context) {
+        return BlocListener<MatchBloc, MatchState>(
+          listener: (context, state) {
+            switch (state.status) {
+              case MatchStatus.loading:
+                LoadingDialog.show(context);
+                break;
+              case MatchStatus.error:
+                LoadingDialog.hide(context);
+                ErrorDialog.show(context, state.error?.message ?? "Une erreur est survenue...");
+                break;
+              case MatchStatus.success:
+                LoadingDialog.hide(context);
+                break;
+              case MatchStatus.initial:
+                LoadingDialog.hide(context);
+                break;
+              case MatchStatus.refresh:
                 BlocProvider.of<MatchBloc>(context).add(GetMatches());
-                if (redirection != null) {
-                  Navigator.push(context, FmiScreen.route(redirection));
-                }
-              });
-              break;
-          }
-        },
-        child: Scaffold(
-          body: SafeArea(
-            child: BlocBuilder<MatchBloc, MatchState>(
-              builder: (context, state) {
-                return Column(
-                  children: [
-                    if (state.previousMatch != null &&
-                        state.previousMatch!.isNotEmpty)
-                      Expanded(
-                          child: MatchSection(
-                              title: "Matchs passés",
-                              matches: state.previousMatch!)),
-                    if (state.nextMatch != null &&
-                        state.nextMatch!.isNotEmpty)
-                      Expanded(
-                          child: MatchSection(
-                              title: "Matchs à venir",
-                              matches: state.nextMatch!)),
-                    if ((state.nextMatch == null ||
-                        state.nextMatch!.isEmpty) && (state.previousMatch == null ||
-                        state.previousMatch!.isEmpty))
-                      Expanded(
-                        child: Center(
-                          child: Text("Aucun match pour le moment"),
+                break;
+              case MatchStatus.redirect:
+                final redirection = state.redirection;
+                //LoadingDialog.show(context);
+                Future.delayed(Duration(seconds: 2), () {
+                  BlocProvider.of<MatchBloc>(context).add(GetMatches());
+                  if (redirection != null) {
+                    Navigator.push(context, FmiScreen.route(redirection));
+                  }
+                });
+                break;
+            }
+          },
+          child: Scaffold(
+            body: SafeArea(
+              child: BlocBuilder<MatchBloc, MatchState>(
+                builder: (context, state) {
+                  return Column(
+                    children: [
+                      if (state.previousMatch != null &&
+                          state.previousMatch!.isNotEmpty)
+                        Expanded(
+                            child: MatchSection(
+                                title: "Matchs passés",
+                                matches: state.previousMatch!)),
+                      if (state.nextMatch != null &&
+                          state.nextMatch!.isNotEmpty)
+                        Expanded(
+                            child: MatchSection(
+                                title: "Matchs à venir",
+                                matches: state.nextMatch!)),
+                      if ((state.nextMatch == null ||
+                          state.nextMatch!.isEmpty) && (state.previousMatch == null ||
+                          state.previousMatch!.isEmpty))
+                        Expanded(
+                          child: Center(
+                            child: Text("Aucun match pour le moment"),
+                          ),
                         ),
-                      ),
-                  ],
-                );
-              },
+                    ],
+                  );
+                },
+              ),
             ),
           ),
-        ),
-      );
-    });
+        );
+      }),
+    );
   }
 }
 
